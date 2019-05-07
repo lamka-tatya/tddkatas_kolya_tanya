7550b9c7b2b679da302bedf95289d07cdf9387d4
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-05-07 18:23:23 +0300

    refactoring

--- TddKatas/Anagrams.cs
+++ TddKatas/Anagrams.cs
@@ -11,32 +11,27 @@ namespace TddKatas
             {
                 return new List<string> {word};
             }
-            
-            if (word == "biro")
-            {
-                return new List<string>
-                {
-                    "biro", "bior", "brio", "broi", "boir", "bori",
-                    "ibro", "ibor", "irbo", "irob", "iobr", "iorb",
-                    "rbio", "rboi", "ribo", "riob", "roib", "robi",
-                    "obir", "obri", "oibr", "oirb", "orbi", "orib"
-                };
-            }
 
-            if (word.Length == 3)
+            if (word.Length == 4)
             {
                 var firstLetter = word[0].ToString();
                 word = word.Substring(1);
                 var result = new List<string>();
 
-                foreach (var anagram in GetAnagramsFromTwoLetters(word))
+                foreach (var anagram in GetAnagramsForThreeLetters(word))
                 {
                     result.Add(anagram.Insert(0, firstLetter));
                     result.Add(anagram.Insert(1, firstLetter));
                     result.Add(anagram.Insert(2, firstLetter));
+                    result.Add(anagram.Insert(3, firstLetter));
                 }
 
                 return result;
+            }
+
+            if (word.Length == 3)
+            {
+                return GetAnagramsForThreeLetters(word);
             }
 
             if (word.Length == 2)
@@ -45,6 +40,22 @@ namespace TddKatas
             }
 
             return new List<string>();
+        }
+
+        private static List<string> GetAnagramsForThreeLetters(string word)
+        {
+            var firstLetter = word[0].ToString();
+            word = word.Substring(1);
+            var result = new List<string>();
+
+            foreach (var anagram in GetAnagramsFromTwoLetters(word))
+            {
+                result.Add(anagram.Insert(0, firstLetter));
+                result.Add(anagram.Insert(1, firstLetter));
+                result.Add(anagram.Insert(2, firstLetter));
+            }
+
+            return result;
         }
 
         private static List<string> GetAnagramsFromTwoLetters(string word)
