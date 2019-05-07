f6f264669d59f218a63b9ca7b6af003f6ee49055
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-05-07 17:04:08 +0300

    refactoring

--- TddKatas.Tests/AnagramsTests.cs
+++ TddKatas.Tests/AnagramsTests.cs
@@ -9,26 +9,6 @@ namespace Tests
 {
     public class AnagramsTests
     {
-        [Test]
-        public void WhenPassed_Word_A_Then_ShouldGenerate_CollectionWithSingle_Word_A()
-        {
-            var word = "a";
-            
-            var result = Anagrams.Generate(word);
-
-            CollectionAssert.AreEqual(result, new[] {"a"});
-        }
-
-        [Test]
-        public void WhenPassed_Word_B_Then_ShouldGenerate_CollectionWithSingle_Word_B()
-        {
-            var word = "b";
-            
-            var result = Anagrams.Generate(word);
-            
-            CollectionAssert.AreEqual(result, new[] {"b"});
-        }
-
         [TestCaseSource(nameof(_oneLetterWords))]
         public void WhenPassed_OneLetter_Then_ShouldGenerate_CollectionWithSameLetter(char letter)
         {
