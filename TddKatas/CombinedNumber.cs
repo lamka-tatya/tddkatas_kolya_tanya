3bb48913394b0952d3a7f3ab8837d73cece63b9e
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-05-14 17:43:56 +0300

    refactoring

--- TddKatas/CombinedNumber.cs
+++ TddKatas/CombinedNumber.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TddKatas
 {
@@ -8,14 +10,23 @@ namespace TddKatas
         {
             if (numList.Count == 3)
             {
-                if (numList[0] == 5 && numList[1] == 6 && numList[2] == 7)
+                var firstNum = numList[0];
+                var secondNum = numList[1];
+                var thirdNum = numList[2];
+
+                var cases = new List<int>()
                 {
-                    return "765";
-                }
-                
-                return "321";
+                    int.Parse(firstNum.ToString() + secondNum.ToString() + thirdNum.ToString()),
+                    int.Parse(secondNum.ToString() + firstNum.ToString() + thirdNum.ToString()),
+                    int.Parse(thirdNum.ToString() + secondNum.ToString() + firstNum.ToString()),
+                    int.Parse(thirdNum.ToString() + firstNum.ToString() + secondNum.ToString()),
+                    int.Parse(secondNum.ToString() + thirdNum.ToString() + firstNum.ToString()),
+                    int.Parse(firstNum.ToString() + thirdNum.ToString() + secondNum.ToString())
+                };
+
+                return cases.Max().ToString();
             }
-            
+
             if (numList.Count == 2)
             {
                 var firstNum = numList[0];
@@ -24,7 +35,7 @@ namespace TddKatas
                 var firstCase = int.Parse(firstNum.ToString() + secondNum.ToString());
                 var secondCase = int.Parse(secondNum.ToString() + firstNum.ToString());
 
-                return (firstCase > secondCase ? firstCase : secondCase).ToString();
+                return Math.Max(firstCase, secondCase).ToString();
             }
 
             if (numList.Count == 1)
