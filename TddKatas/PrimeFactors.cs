a7dda9d708fcc0981dfb3c00bc7de05097eb16fc
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-05-15 16:55:06 +0300

    refactoring

--- TddKatas/PrimeFactors.cs
+++ TddKatas/PrimeFactors.cs
@@ -4,17 +4,7 @@ namespace TddKatas
     {
         public static int[] Get(int number)
         {
-            if (number == 3)
-            {
-                return new[] {3};
-            }
-            
-            if (number == 5)
-            {
-                return new[] {5};
-            }
-            
-            return new[] {2};
+            return new[] {number};
         }
     }
 }
