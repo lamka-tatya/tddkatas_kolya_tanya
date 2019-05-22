561161d65f8b4b53918845d212e4363d18bbdc64
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-05-22 18:10:31 +0300

    coordinate

---
+++ TddKatas/TicToc/Space.cs
@@ -0,0 +1,15 @@
+namespace TddKatas.TicToc
+{
+    public struct Space
+    {
+        
+        
+        public Space( Coordinate[] coordinates)
+        {
+            Coordinates = coordinates;
+        }
+        
+        public readonly Coordinate[] Coordinates;
+        
+    }
+}
